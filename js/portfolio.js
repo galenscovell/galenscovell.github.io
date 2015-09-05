e2e9dd28caf146543dd2cd6da3e9c885d963bf62
Galen Scovell <galen.scovell@gmail.com>
2015-09-05 12:09:56 -0700

    Update skills and current job info, etc.

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,30 +1,7 @@
 
 
-$(document).ready(function() {
-
-    // Smooth scroll on nav click
-    $('a[href^="#"]').on('click',function (event) {
-        event.preventDefault();
-
-        var target = this.hash;
-        $target = $(target);
-
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top
-        }, 800, 'swing', function () {
-            window.location.hash = target;
-        });
-    });
-
-
-});
-
-// If user has javascript, setup fade effects
-$('.about_row div').css('opacity', '0');
-$('.skills_row div').css('opacity', '0');
-
-// Fade in effect when objects reached on scroll
 $(window).scroll(function(event) {
+    // Fade in effect when objects reached on scroll
     $('.about_row div').each(function(i, el) {
         var el = $(el);
         if (el.visible(true)) {
@@ -38,4 +15,22 @@ $(window).scroll(function(event) {
             el.animate({'opacity':'1'},800);
         }
     });
+});
+
+$('a[href^="#"]').on('click',function (e) {
+    // Swing animation on link click
+    e.preventDefault();
+    var target = this.hash;
+    $target = $(target);
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 800, 'swing', function () {
+        window.location.hash = target;
+    });
+});
+
+
+$(document).ready(function() {
+    $('.about_row div').css('opacity', '0');
+    $('.skills_row div').css('opacity', '0');
 });
