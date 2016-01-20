3b5428984b7421e9e22dce7a70eaac14ffb24afa
Galen <galen.scovell@gmail.com>
2016-01-19 19:52:31 -0800

    Don't change address on location swing

--- js/portfolio.js
+++ js/portfolio.js
@@ -11,8 +11,6 @@ $('a[href^="#"]').on('click', function (e) {
     var target = this.hash;
     $target = $(target);
     $('html, body').stop().animate({
-        'scrollTop': $target.offset().top
-    }, 500, 'swing', function () {
-        window.location.hash = target;
-    });
+        'scrollTop': $target.offset().top - 60
+    }, 500, 'swing');
 });
